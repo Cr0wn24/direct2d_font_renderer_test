d0f54b7512a4e14fe27c78f064bc053c95fdeec7
Hampus Johansson <61281897+Cr0wn24@users.noreply.github.com>
2024-11-13 19:45:15 +0100

    added weird case of MapCharacters() not doing correct font fallbacking

--- d2d_text_rendering_example.cpp
+++ d2d_text_rendering_example.cpp
@@ -247,19 +247,19 @@ WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR command_line, int show
   //----------------------------------------------------------
   // hampus: map text to glyphs
 
-  const wchar_t *ligatures_text = L"Ligatures: !=>=-><-=><=";
-  const wchar_t *emojis_text = L"emojis: 🤷‍♂️";
-  const wchar_t *text = L"Hello world";
-  const wchar_t *arabic_text = L"Arabic text: مرحبا بالعالم";
+  // const wchar_t *ligatures_text = L"Ligatures: !=>=-><-=><=";
+  const wchar_t *emojis_text = L"😀a";
+  // const wchar_t *text = L"Hello world";
+  // const wchar_t *arabic_text = L"Arabic text: مرحبا بالعالم";
 
-  const wchar_t *font = L"Segoe UI";
+  const wchar_t *font = L"Consolas";
 
   MapTextToGlyphsResult text_to_glyphs_results[] =
   {
-    dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, ligatures_text, wcslen(ligatures_text)),
+    // dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, ligatures_text, wcslen(ligatures_text)),
     dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, emojis_text, wcslen(emojis_text)),
-    dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, text, wcslen(text)),
-    dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, arabic_text, wcslen(arabic_text)),
+    // dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, text, wcslen(text)),
+    // dwrite_map_text_to_glyphs(font_fallback1, font_collection, text_analyzer1, &locale[0], font, 50.0f, arabic_text, wcslen(arabic_text)),
   };
 
   wchar_t filePaths[8][MAX_PATH] = {};
